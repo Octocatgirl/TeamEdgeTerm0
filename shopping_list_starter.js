01f7f51ff0d0a76230b7c5bd5e5df8db262b4a25
Student <student@m-fvfzq18hlywp.lan>
2020-07-14 16:04:34 -0700

    Updated

--- shopping_list_starter.js
+++ shopping_list_starter.js
@@ -27,53 +27,94 @@ console.log(`
 `)
 
 let welcomeMessage = `Hi! I'm your shopping assistant. Let me take your order. \n
-You can type 'add milk' to add milk to your shopping list. \n 
-or you can type 'remove milk' to remove it. \n`
+To add and item you can type 'add {item}'. \n
+For example,'add milk' to add milk to your shopping list. \n 
+To remove an item you can typ you can type 'remove {item}. \n
+For example, 'remove milk' to remove milk form your shopping list. \n
+To check your list type 'check list' and you can see what you currently have on your list. \n
+To end your list input 'end list'. \n`
 
 console.log(welcomeMessage)
 
-
-//-->Todo: declare a shoppingList array
 
 let shoppingList = []
 let reply = ""
 
 while (active == true) {
-
-    checkAnswer(promptUser()) //this makes the program continously prompt and check response while the boolean 'active' returns true
-
+    checkAnswer(promptUser())
 }
 
 function promptUser() {
 
-    let reply = READLINE.question("What do you want to add or remove? (no more items? input 'end list')  >>  ").toLowerCase
-
+    reply = READLINE.question("What do you want to add or remove? (no more items? type 'end list')  >>  ").toLowerCase()
     return reply
 
 }
 
-function checkAnswer(reply) {
+
+function checkAnswer() {
     let answer = reply.split(" ")
-    console.log(answer)
-    if (answer[0] != "remove" && answer[0] != "add") {
+
+    if (answer[0] != "remove" && answer[0] != "add" && answer[0] != "end" && answer[0] != "check" && answer[1] != "list") {
         console.log("*invalid action*")
         active = true
-    } else if (anwer[1] == undefined) {
-        console.log("*You have given no item to add or remove*")
-        active = truee
-    } else if (answer[0] == "end" && anserw[1] == "list") {
-        active = false
-    }
-}
-
+    } else if (answer[1] == undefined) {
+        console.log("*You have given no item to add or remove*");
+        active = true;
+    } else if (answer[0] == "end" && answer[1] == "list") {
+        Currentlist()
+        ending = READLINE.question(`\n\nAre you sure you want to end your list? Do you have everything your want in your list? : `).toLowerCase()
+        if (ending == "yes") {
+            console.log("\n< ending list >\n");
+            endList();
+        } else if (ending == "no") {
+            console.log("\n Did you realized that you need to add somthing else? That's fine take your time.\nNo need to rush, make sure your list is complete. :)\n")
+            active = true;
+        } else if (ending != "no" && ending != "yes") {
+            console.log("\nSorry I don't understand your answer. \nI'm just going to assume that you want to continue your list.\n")
+            active = true
+        }
+    } else if (answer[0] == "add" && answer[1] != undefined && answer[1] != "list" && answer[2] == undefined) {
+    addItem()
+} else if (answer[0] == "remove" && answer[1] != undefined && answer[1] != "list" && answer[2] == undefined) {
+    removeItem()
+} else if (answer[0] == "check" && answer[1] == "list") {
+    Currentlist();
+} else if (answer[2] != undefined) {
+    console.log("Too many items please input items 1 at a time. \nIf the item contains two words please put them together with no space. \nFor Example: 'Potatoe chips' would be written as 'PotatoeChips'. \n")
+};
+};
 
 function addItem() {
-    let item = reply.split;
+    let item = reply.split(" ");
     item.shift();
-    reply.push
+    shoppingList.push(item[0])
+    console.log(`\n < [${item}] has been added > \n`)
 }
 
 function removeItem() {
-    reply.includes()
+    let item = reply.split(" ");
+    item.shift()
+    index = shoppingList.indexOf(item[0]);
+    if (index >= 0) {
+        shoppingList.splice(index, 1)
+        console.log(`\n < [${item}] has been removed >\n`)
+    } else {
+        console.log(`[${item}] does not exist in your list. \nThere is nothing to remove.\n`)
+    }
 
 }
+
+function endList() {
+    shoppingListjoined = shoppingList.join("\n- ")
+    console.log("\nThank You for using our services!\nHere is your list:\n______________________________\n\n- " + shoppingListjoined + "\n\nHave a nice day!")
+    active = false
+}
+
+function Currentlist() {
+    if (shoppingList.length == 0) { console.log("\nYou have nothing in your list, you need to add something.\nTry typing 'add chips'.\n"); } else {
+        shoppingListjoined = shoppingList.join("\n- ");
+        console.log("Your current shopping list: \n- " + shoppingListjoined);
+    }
+}
+
--- while_loops.js
+++ while_loops.js
@@ -33,26 +33,39 @@ ww      ww hhhhhh  iii lll ee   e    lll oo  oo oo  oo ppp  pp s
 
 console.log("------------------- CHALLENGE 1 : IN YOUR PRIME -------------------")
 
- //Here is a humble while loop in action. We need a variable to hold the counter value.
+//Here is a humble while loop in action. We need a variable to hold the counter value.
 let num = 0
-while(num <= 10){
+while (num < 11) {
 
-console.log("example counter--> " + num)
-num++
+    console.log("example counter--> " + num)
+    num++
 }
 //-->TODO: Declare a while loop that prints all the prime numbers between 0 and 1000
 
-
+let loop = 0
+while (loop <= 1000) {
+    console.log("Zero to Onehundred: " + loop);
+    loop++;
+}
 
 console.log("------------------- CHALLENGE 2 : FOUND   -------------------")
 
 //here is an array full of items
-let items = ["pencil" , "eraser" , "mirror" , "comb" , "spoon" , "key" , "earrings" ,"cat food" , "magazine"]
+let items = ["pencil", "eraser", "mirror", "comb", "spoon", "key", "earrings", "cat food", "magazine"]
 
 //-->TODO: Use a while loop to search the contents of an array for the key! If it exists, print "found the key!"
-
-
-
+//items.includes("key") == true returns true if include
+let i = 0
+let keyfound = false
+while (!keyfound) {
+    if (items[i] == "key") {
+        console.log("key had been found!")
+        keyfound = true
+    } else {
+        console.log("This item is a " + items[i]);
+        i++
+    }
+}  //having issues...
 
 
 console.log("------------------- CHALLENGE 3 : BUGGIN   -------------------")
@@ -62,13 +75,12 @@ console.log("------------------- CHALLENGE 3 : BUGGIN   -------------------")
 
 //-->TODO: Make me count  2, 4, 6,..., 50
 
-function evenNumbersToFifty(){
+function evenNumbersToFifty() {
 
-    let num = 50
-    while(num <50){
-
+    let num = 2
+    while (num <= 50) {
         console.log("number: " + num)
-
+        num = num + 2
     }
 }
 evenNumbersToFifty()
@@ -89,17 +101,22 @@ evenNumbersToFifty()
 
 
 
-function pattern(){
+function pattern() {
 
-    let index = 0 
-    let array =[]
-    
-    while(index <= 5 ){
-      
+    let index = 0
+    let array = []
+
+    while (index <= 5) {
+
         array.push(index)
-    
-       // console.log(array)
+
+        console.log(array)
         index++
+    }
+    while (index > 0) {
+        console.log(array)
+        array.pop(index)
+        index--
     }
 
 }
@@ -124,21 +141,26 @@ console.log("------------------- CHALLENGE 5 : WHAT AM I?   -------------------"
 //         Notice how one function calls the other and uses the returned value as the input. This is called Recursion! 
 
 let keepAsking = false
-while(keepAsking){
-
- //response(promptUser())
+let x = READLINE.question("What am I? ")
+while (keepAsking == false) {
+    promptUser(response())
+    checkResponse()
 }
 
-function promptUser(){
+function promptUser() {
+    x = READLINE.question("What am I? ")
+}
 
-  
+function response() {
+    console.log(`I know you are a ${x}, What am I?? `)
 
 }
 
-function response(response){
-
-  
-
+function checkResponse(){
+    if (x === "ChiliCat"){
+        console.log("Wait... I am a ChiliCat! :O ")
+        keepAsking = true 
+    }
 }
 
 //-->TODO: Challenge! write a secret word to break out of the loop!
